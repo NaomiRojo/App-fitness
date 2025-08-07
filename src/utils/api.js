1876fabf8c9e2b895251de4ce92cdae877d4a4e6
Naomi Andrea Rojo Mejia <142241042+NaomiRojo@users.noreply.github.com>
2025-08-07 16:22:25 -0400

    Update api.js

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,13 +1,13 @@
 import axios from 'axios';
 
-// Función para registrar un usuario usando Reqres API
+
 export const registerUser = async (email, password) => {
   try {
     const response = await axios.post('https://reqres.in/api/register', {
       email,
       password,
     });
-    return response.data; // Devuelve los datos de la respuesta
+    return response.data;
   } catch (error) {
     throw error.response
       ? error.response.data.error
